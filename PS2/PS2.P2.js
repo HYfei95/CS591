f77e5e59cef1e2c8e31fcc65a88502a6dab91c68
Hongy F <peterhongyaofei@gmail.com>
2019-05-30 15:30:12 -0400

    p2

---
+++ PS2/PS2.P2.js
@@ -0,0 +1,12 @@
+function* wordParser(str) {
+    yield*  str.split(' ');
+}
+
+const str = `All I know is something like a bird within her sang`;
+let word = wordParser(str);
+let result = word.next();
+
+while(result.done === false){
+    console.log(result.value);
+    result = word.next();
+}
