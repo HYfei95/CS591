1d9439a16f955993a59d0f91edb60138264d266c
Hongy F <peterhongyaofei@gmail.com>
2019-05-29 23:55:26 -0400

    p2

---
+++ PS2/PS2.P1.js
@@ -0,0 +1,18 @@
+function* fibs () {
+    let [val1, val2, result] = [0, 1, 0];
+    while (true) {
+        result = val1+val2;
+        val1 = val2;
+        val2 = result;
+        yield result;
+    }
+}
+
+
+
+//Get a few fibs
+myFibs = fibs();
+let count = 0;
+while (count --> 0) {
+    console.log(myFibs.next().value)
+}
