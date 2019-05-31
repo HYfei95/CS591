fd2d7977d5ed474adc516561d25ebb40f45c11e9
Hongy F <peterhongyaofei@gmail.com>
2019-05-31 11:53:11 -0400

    p2

--- PS2/PS2.P1.js
+++ PS2/PS2.P1.js
@@ -1,18 +1,21 @@
-function* fibs () {
-    let [val1, val2, result] = [0, 1, 0];
+function* fibs (x) {
+    let [val1, val2, result] = [x,x-1,0];
+    if (!x) {val2 = 1; yield 0;}
+    //    let [val1, val2, result] = [0, 1, 0]
     while (true) {
-        yield result;
         result = val1+val2;
         val1 = val2;
         val2 = result;
+        if (result > Number.MAX_SAFE_INTEGER) throw new Error()
+        yield result
     }
 }
 
 
-function* evenFib(num){
-    let myFib = fibs();
+function* evenFib(){
+    let myFib = fibs(0);
     while(true){
-         num = myFib.next().value;
+        let num = myFib.next().value;
         if(num === 0 || num%2 === 0){
             yield num;
         }
@@ -21,7 +24,7 @@ function* evenFib(num){
 
 let result = evenFib();
 const print = count => {
-    while (count-- > 0) {
+    while (count --> 0) {
         console.log(result.next().value)
     }
 };
