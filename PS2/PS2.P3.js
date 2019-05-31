7c0a641e9b5fea6b4b65ec140a7868433c96e5ec
Hongy F <peterhongyaofei@gmail.com>
2019-05-31 12:01:58 -0400

    ps2

--- PS2/PS2.P3.js
+++ PS2/PS2.P3.js
@@ -1,5 +1,5 @@
 const cube = x => x**3;
-const cubeMap = num => num.map( x => cube(x));
+const cubeMap = num => num.map(x => cube(x));
 
 let array = [1,2,3,4,5,6,7];
 console.log(`Cube value of list ${array} are ${cubeMap(array)}`);
